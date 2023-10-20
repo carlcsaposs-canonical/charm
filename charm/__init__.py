48fac427d11e8c63690b0138a7927704ba950966
Carl Csaposs <carl.csaposs@canonical.com>
2023-10-20 08:35:19 +0000

    Add example property for `unit_status`

--- charm/__init__.py
+++ charm/__init__.py
@@ -1,1 +1,41 @@
+import subprocess
+import typing
+
 from ._status import *
+
+
+class _FooExample:
+    # example getter/setter for module property
+
+    _STATUS_CODES: dict[type[Status], str] = {
+        ActiveStatus: "active",
+        WaitingStatus: "waiting",
+        MaintenanceStatus: "maintenance",
+        BlockedStatus: "blocked",
+    }
+
+    @property
+    def unit_status(self) -> typing.Optional[Status]:
+        result = json.loads(
+            subprocess.run(
+                ["status-get", "--include-data" "--format", "json"],
+                check=True,
+                capture_output=True,
+            ).stdout
+        )
+        status_types: dict[str, type[Status]] = {
+            code: status for status, code in self._STATUS_CODES.items()
+        }
+        if status_type := status_types.get(result["status"]):
+            return status_type(result["message"])
+
+    @unit_status.setter
+    def unit_status(self, value: Status) -> None:
+        subprocess.run(
+            ["status-set", self._STATUS_CODES[type(value)], str(value)], check=True
+        )
+
+
+
+unit_status = _FooExample().unit_status
+app_status: typing.Optional[Status] = None
